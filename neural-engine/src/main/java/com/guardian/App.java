ca9e64a87f95df06deafc19e9990e50f03ffd270
Sophia Jung <sophiajung@Sophias-MacBook-Pro.local>
2023-07-01 02:08:12 -0700

    added slf4j dependencies to pom.xml

--- neural-engine/src/main/java/com/guardian/App.java
+++ neural-engine/src/main/java/com/guardian/App.java
@@ -2,8 +2,12 @@ package com.guardian;
 
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClientBuilder;
+import software.amazon.awssdk.core.exception.SdkException;
 
 import static software.amazon.awssdk.services.dynamodb.DynamoDbClient.*;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 
 //import software.amazon.awssdk.services.dynamodb.*;
 
@@ -16,5 +20,7 @@ public class App
     public static void main( String[] args )
     {
         DynamoDbClient dbClient = DynamoDbClient.create();
+        DefaultCredentialsProvider cred = DefaultCredentialsProvider.create();
+        System.out.println(cred.toString());
     }
 }
