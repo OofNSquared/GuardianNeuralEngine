ba7e21f63cde4f633359d98af99c4fc0b217397e
Sophia Jung <sophiajung@Sophias-MBP.attlocal.net>
2023-07-03 23:22:18 -0700

    added abstract request() method to superclass, moved getKeyMap() method to superclass

--- neural-engine/src/main/java/com/guardian/GuardianWorkflows/userworkflows/UserRequests.java
+++ neural-engine/src/main/java/com/guardian/GuardianWorkflows/userworkflows/UserRequests.java
@@ -1,5 +1,10 @@
 package com.guardian.GuardianWorkflows.userworkflows;
 
+import java.util.HashMap;
+
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbRequest;
+
 public abstract class UserRequests {
 
     private int id;
@@ -7,10 +12,10 @@ public abstract class UserRequests {
 
 
     public UserRequests(int id, String email) {
+        if (isNullOrBlank(email)) throw new IllegalArgumentException("email cannot be null or blank");
         this.id = id;
         this.email = email;
     }
-
 
     public int getId() {
         return this.id;
@@ -25,12 +30,21 @@ public abstract class UserRequests {
     }
 
     public void setEmail(String email) {
+        if (isNullOrBlank(email)) throw new IllegalArgumentException("email cannot be null or blank");
         this.email = email;
     }
 
-    protected boolean isNullOrBlank(String str) {
+    private boolean isNullOrBlank(String str) {
         return (str == null || str.isBlank());
     }
+
+    public HashMap<String, AttributeValue> getKeyMap() {
+        HashMap<String, AttributeValue> keyToGet = new HashMap<String, AttributeValue>();
+        keyToGet.put("ID", AttributeValue.builder().n(String.valueOf(this.id)).build());
+        keyToGet.put("email", AttributeValue.builder().s(this.email).build());
+        return keyToGet;
+    }
     
+    public abstract DynamoDbRequest request();
 
 }
